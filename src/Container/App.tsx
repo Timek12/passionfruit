5d1bf5d3efc0f2fc8ce43f730aa477599ff2a9d3
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-18 19:56:03 +0100

    Add menuItems state and create fetch request to retrive all menu items

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Footer, Header } from "../Components/Layout";
-
+import { useState, useEffect } from "react";
+import { API_PATH } from "../constants";
+import { menuItemModel } from "../Interfaces";
 
 function App() {
+  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+
+  useEffect(() => {
+    fetch(API_PATH + "MenuItem").then((response) => response.json()).then((data) => {
+      console.log(data);
+      setMenuItems(data.result);
+    })
+  }, [])
+
   return (
     <div className="text-white">
       <Header />
