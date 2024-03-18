594c220b5cd4ddf86d9aabfaabe5c0c8f3362d2e
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-18 20:16:10 +0100

    Moe menuItems state and fetch endpoint to separate controller

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import { Footer, Header } from "../Components/Layout";
-import { useState, useEffect } from "react";
-import { API_PATH } from "../constants";
-import { menuItemModel } from "../Interfaces";
+import { Home } from "../Pages";
 
 function App() {
-  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
-
-  useEffect(() => {
-    fetch(API_PATH + "MenuItem").then((response) => response.json()).then((data) => {
-      console.log(data);
-      setMenuItems(data.result);
-    })
-  }, [])
-
   return (
     <div className="text-white">
       <Header />
-      Main Component
+      <Home />
       <Footer />
     </div>
   );
