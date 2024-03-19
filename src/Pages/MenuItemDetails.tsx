ac10437464a5e58e18f7ea952135f03d4aed48f9
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-19 18:36:17 +0100

    Add funcitonality to dynamically set quantity of a menu item

--- src/Pages/MenuItemDetails.tsx
+++ src/Pages/MenuItemDetails.tsx
@@ -15,9 +15,19 @@ import { useNavigate } from "react-router-dom";
 function MenuItemDetails() {
   const { menuItemId } = useParams();
   const navigate = useNavigate();
+  const [quantity, setQuantity] = useState(1);
 
   const { data, isLoading } = useGetMenuItemByIdQuery(menuItemId);
 
+  const handleQuantity = (counter: number) => {
+    let newQuantity = quantity + counter;
+    if(newQuantity < 1) {
+      newQuantity = 1;
+    }
+
+    setQuantity(newQuantity);
+    return;
+  }
   return (
     <div className="container pt-4 pt-md-5">
       {isLoading ? (
@@ -57,12 +67,12 @@ function MenuItemDetails() {
                 className="pb-2  p-3"
                 style={{ border: "1px solid #333", borderRadius: "30px" }}
               >
-                <i
+                <i onClick={() => {handleQuantity(-1)}}
                   className="bi bi-dash p-1"
                   style={{ fontSize: "25px", cursor: "pointer" }}
                 ></i>
-                <span className="h3 mt-3 px-3">XX</span>
-                <i
+                <span className="h3 mt-3 px-3">{quantity}</span>
+                <i onClick={() => {handleQuantity(+1)}}
                   className="bi bi-plus p-1"
                   style={{ fontSize: "25px", cursor: "pointer" }}
                 ></i>
